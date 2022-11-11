02561aeaef9cc7167dfc10f9e3dd4bec899a0cd2
Michael Pilosov <consistentbayes@gmail.com>
2022-11-11 09:26:33 +0000

    cleanup binder setup

---
+++ binder/appendix.py
@@ -0,0 +1,9 @@
+
+# start mlflow in background
+os.system("make serve")
+
+# binder-specific features
+with st.sidebar:
+    st.markdown('[launch mlflow](../mlflow/)')
+    if st.button('Create New Run'):
+        os.system('python fill.py')
