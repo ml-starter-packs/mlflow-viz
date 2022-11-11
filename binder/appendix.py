cfadcca376868b96d2d7402296856c92dea212b6
Michael Pilosov <consistentbayes@gmail.com>
2022-11-11 09:39:24 +0000

    add option to edit app in binder

--- binder/appendix.py
+++ binder/appendix.py
@@ -5,5 +5,6 @@ os.system("make serve")
 # binder-specific features
 with st.sidebar:
     st.markdown('[launch mlflow](../mlflow/)')
+    st.markdown('[edit app (live!)](../lab/)')
     if st.button('Create New Run'):
         os.system('python fill.py')
