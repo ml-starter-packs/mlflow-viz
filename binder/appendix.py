1618df8fd25c277dd4d54b69416d292a4bea82c6
Michael Pilosov <consistentbayes@gmail.com>
2022-11-11 09:42:25 +0000

    no subprocess

--- binder/appendix.py
+++ binder/appendix.py
@@ -1,11 +1,7 @@
 
-# start mlflow in background
-import subprocess
-subprocess.run(["make", "serve"])
-
 # binder-specific features
 with st.sidebar:
-    st.markdown('[launch mlflow](../mlflow/)')
+    st.markdown('[launch mlflow (may require refresh)](../mlflow/)')
     st.markdown('[edit app (live!)](../lab/)')
     if st.button('Create New Run'):
         os.system('python fill.py')
