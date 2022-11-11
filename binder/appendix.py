82017f4e995ee50dc859c30a1afe858820cf5ecf
Michael Pilosov <consistentbayes@gmail.com>
2022-11-11 09:45:56 +0000

    update link

--- binder/appendix.py
+++ binder/appendix.py
@@ -2,6 +2,6 @@
 # binder-specific features
 with st.sidebar:
     st.markdown('[launch mlflow (may require refresh)](../mlflow/)')
-    st.markdown('[edit app (live!)](../lab/)')
+    st.markdown('[edit app (live!)](../lab/tree/app.py)')
     if st.button('Create New Run'):
         os.system('python fill.py')
